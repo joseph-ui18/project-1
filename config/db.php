b49c34bbcffb2630f536315bf7f15c3ae8a88d5f
joseph-ui18 <josephbenjamindeleon15@gmail.com>
2026-02-15 14:28:26 +0800

    db.php fixed

--- config/db.php
+++ config/db.php
@@ -2,9 +2,19 @@
 $host = 'localhost';
 $db   = 'airport_db';
 $user = 'root';
+$pass2= '';
 $pass = 'carl';
+
+
 $conn = new mysqli($host, $user, $pass, $db, 3307);
+
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+
+    $conn = new mysqli($host, $user, $pass2, $db, 3306);
+
+    if ($conn->connect_error) {
+        
+        die("Connection failed on both ports: " . $conn->connect_error);
+    }
 }
 ?>
